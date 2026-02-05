792579daf8c7d1fec19820cce94efea9e7455998
Manrique1601836 <manriqueedwin558@gmail.com>
2026-02-05 18:08:40 -0500

    cversion 2

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -2,9 +2,14 @@
 import "./style/view.css"
 
 import Home from "./home"
+import LogoTipo from "./logo"
 
 export default function Views(){
     return(
-        <Home/>
+        <>
+            <Home/>
+            <LogoTipo/>
+        </>
+
     )
 }
---
+++ src/pages/logo.jsx
@@ -0,0 +1,22 @@
+
+import Logo from "../img/logo.png"
+import Tonny from "../video/Tonny.mp4"
+
+export default function LogoTipo(){
+    return(
+        <div className="logoK">
+            <div className="logo-1">
+                {/* <img src={Logo} alt="logo-skater" className="logo-skater"/> */}
+                <h1>KickWave</h1>
+            </div>
+            <div className="logo-2">
+                <video 
+                    value={Tonny} 
+                    controls
+                    loop
+                    className="media-a"
+                />
+            </div>
+        </div>
+    )
+}
